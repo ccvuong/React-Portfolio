3dc9b3865ec11a45f7c3b3c6c2a38b11fef608da
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-13 23:42:32 -0700

    creating navbar js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Resume from './pages/Resume';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        {/* navbar will exist on every page */}
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../styles/Navbar.css";
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+
+            <div className='toggleButton'>
+                <button> </button>
+            </div>
+            <div className='links'>
+                <Link to="/"> Home </Link>
+                <Link to="/projects"> Projects </Link>
+                <Link to="/about"> About </Link>
+                <Link to="/resume"> Resume </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
