542664195aac3906f9ae9d67aa046786e2b608a3
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-14 01:12:00 -0700

    updating Home files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className='home'>
+      <div className='about'>
+        <h2> Hey there, I'm Christina 😎</h2>
+
+        <div className='prompt'>
+
+          <p>
+            A growing developer with a curious mind in learning the matrix. 
+          </p>
+
+          <p> So, the blue pill 💙 or the red pill ❤️? </p>
+
+        </div>
+      </div>
+      <div className='skills'>
+
+        <h1>Skills</h1>
+
+        <ol className='list'>
+          <li className='item'>
+            <h2>Frontend</h2>
+            <span> ReactJS, HTML, CSS, Material UI</span>
+          </li>
+
+          <li className='item'>
+            <h2>Backend</h2>
+            <span> NodeJS, ExpressJS, MySql</span>
+          </li>
+
+          <li className='item'>
+            <h2>Languages</h2>
+            <span> JavaScript </span>
+          </li>
+
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home
