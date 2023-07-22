4df89dfebf39bd236ab03e7c62f05c5f3cdc6b6b
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-07-21 22:56:02 -0700

    updating portfolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Projects from './pages/Projects';
 import About from './pages/About';
-// import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 import Navbar from './components/Navbar';
@@ -15,11 +14,12 @@ function App() {
       <Router>
         {/* navbar will exist on every page */}
         <Navbar />
+
         <Routes>
           <Route path='/projects' element={<Projects />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
-          {/* <Route path='/resume' element={<Resume />} /> */}
+          
         </Routes>
 
         <Footer />
